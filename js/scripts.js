b19511179fe8813c5f76cee7697e9972a9dbafd7
ddavisgraphics <dave@ddavisgraphics.com>
2017-01-22 20:21:05 -0500

    Trying to use the API.  ZIp codes do not work.
    THis API is difficult.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(function(){
   // this will take some time to populate, so might want to think about some call back here.
   // var userInfo = getLatLong('26554');
 
-  var data = testAjaxData('26554');
+  var data = testAjaxData('15201');
 });
 
 
@@ -13,32 +13,32 @@ $(function(){
 // not getting any data back from the supplied zip code yet, but may have errors.
 // if zip code deosn't work, I'll work on passing the information another way.
 function testAjaxData(zipcode){
-  $.ajax({
-    url: 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data',
-    headers: { token: 'QPuEobODufxVhKNDEtwNjxgPcFSwPOWS'},
-    data: {
-      datasetid: 'GHCND',
-      datatypeid: 'TMAX',
-      locationid: 'ZIP:(' + zipcode +  ')',
-      startdate:'2015-02-02', // YYYY-MM-DD
-      enddate: '2015-03-16', // YYYY-MM-DD
-    },
-    success:function(data){
-      console.log(JSON.stringify(data));
-      return data;
-    },
+        $.ajax({
+                type: 'GET',
+                url: 'https://www.ncdc.noaa.gov/cdo-web/api/v2/data',
+                headers: { token: 'QPuEobODufxVhKNDEtwNjxgPcFSwPOWS'},
+                data: {
+                        datasetid: 'GSOM',
+                        datacategoryid: 'TEMP',
+                        locationid: 'FIPS:54',
+                        startdate:'2015-02-02', // YYYY-MM-DD
+                        enddate: '2015-03-16', // YYYY-MM-DD
+                        limit:15
+                },
+                success:function(data){
+                        console.log("JSON --> " + JSON.stringify(data));
+                },
 
-    error: function (jqXHR, textStatus, errorThrown) {
-      if (jqXHR.status == 500) {
-          console.log('Internal error: ' + jqXHR.responseText);
-      } else {
-          console.log('Unexpected error.');
-      }
-    },
+                error: function (jqXHR, textStatus, errorThrown) {
+                        if (jqXHR.status == 500) {
+                          console.log('Internal error: ' + jqXHR.responseText);
+                        } else {
+                          console.log('Unexpected error.');
+                        }
+                },
 
-    complete: function(){
-      console.log('call callback function here')
-    },
-
-  });
+                complete: function(){
+                        console.log('call callback function here')
+                },
+        });
 }
